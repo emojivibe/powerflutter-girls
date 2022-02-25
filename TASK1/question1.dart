0017a96c9176cb02b4981967d559295558cafc07
emojivibe <93862405+emojivibe@users.noreply.github.com>
2022-02-25 21:22:08 +0530

    Create question1.dart

---
+++ TASK1/question1.dart
@@ -0,0 +1,9 @@
+void main() {
+//Change the value of x to check whether number is divisible by 5
+int x = 37;
+if (x % 5 == 0) {
+print('DIVISIBLE');
+} else if (x % 5 != 0) {
+print('NON DIVISIBLE');
+}
+}
